6aa9a733d5af84c399d5b8ac3d9347d923801d10
Justin Winger <justinwinger4@gmail.com>
2020-03-30 16:07:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,17 +3,6 @@ class Dog
   def initialize(name, breed)
     @breed = breed 
   end
-  def name=(name)
-    @name = name
-  end
-  def name
-    @name
-  end
-  def breed=(breed)
-    @breed = breed
-  end
-  def breed
-    @breed
-  end
+  
 end
   
