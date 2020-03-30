5178bff0941f5997ee6d65f96b6e316ec4c2014b
Justin Winger <justinwinger4@gmail.com>
2020-03-30 16:13:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,6 +4,18 @@ class Dog
     @name = name
     @breed = breed 
   end
-  attr_accessor :name, :breed
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end 
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+    
 end
   
